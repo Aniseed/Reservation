dabd952bd60a92063677717cd25a0280ef86a917
Piotrek Matuszkiewicz <piotrek@applicake.com>
2013-02-26 00:21:15 +0100

    Ustawia strone startowa na liste kin

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Reservation::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'kinos#index'
 
   # See how all your routes lay out with "rake routes"
 
